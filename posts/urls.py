807bec440453b1801730bbfb466bdffe3bf0871f
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2024-03-29 10:46:25 +0000

    added crud for posts

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts', views.list_posts, name='homepage'),
-    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    path('posts/<int:pk>/update/', views.update_post, name='post_update'),
-    path('posts/<int:pk>/delete/', views.delete_post, name='post_delete'),
+    path('posts/', views.PostList.as_view(), name='post_list'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    # path('posts/<int:pk>/update/', views.update_post, name='post_update'),
+    # path('posts/<int:pk>/delete/', views.delete_post, name='post_delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,36 +1,14 @@
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from posts.serializers import PostSerializer
-from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
-
-
 from posts.models import Post
 
 
-@api_view(['GET', 'POST'])
-def list_posts(request: Request):
-    posts = Post.objects.all()
-
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            response = {
-                'message': 'post created successfully',
-                'data': serializer.data
-            }
-            return JsonResponse(response, status=status.HTTP_201_CREATED)
-        response = {
-            'message': 'validation error',
-            'error': serializer.errors
-        }
-        return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'GET':
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         response = {
             'data': serializer.data,
@@ -38,54 +16,58 @@ def list_posts(request: Request):
         }
         return Response(response, status=status.HTTP_200_OK)
 
-
-@api_view(['GET'])
-def post_detail(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = PostSerializer(post)
-    response = {
-        'message': 'fetch post detail successfully',
-        'data': serializer.data
-    }
-    return JsonResponse(response, status=status.HTTP_200_OK)
-
-
-@api_view(['PUT'])
-def update_post(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    data = JSONParser().parse(request)
-    serializer = PostSerializer(post, data=data)
-    if serializer.is_valid():
-        serializer.save()
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                'message': 'post created successfully',
+                'data': serializer.data
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
         response = {
-            'message': 'Updated post detail successfully',
+            'message': 'validation error',
+            'error': serializer.errors
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        response = {
+            'message': 'fetch post detail successfully',
             'data': serializer.data
         }
         return JsonResponse(response, status=status.HTTP_200_OK)
-    response = {
-        'message': 'validation error',
-        'error': serializer.errors
-    }
-    return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                'message': 'Updated post detail successfully',
+                'data': serializer.data
+            }
+            return JsonResponse(response, status=status.HTTP_200_OK)
+        response = {
+            'message': 'validation error',
+            'error': serializer.errors
+        }
+        return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['DELETE'])
-def delete_post(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    post.delete()
-    response = {
-        'message': 'Post deleted'
-    }
-    return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        response = {
+            'message': 'Post deleted'
+        }
+        return JsonResponse(response, status=status.HTTP_200_OK)
